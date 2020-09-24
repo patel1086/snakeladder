f5688a1a817bf21a8427e976ee57da0f79a7d01d
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-09-24 11:00:28 +0530

    [Jitendra] Added: Roll a dice and player got random number

--- Snake.java
+++ Snake.java
@@ -2,6 +2,10 @@ public class Snake{
 	public static void main(String [] args){
  	int player=0;
 	System.out.println("Player is at starting point");
-
+	int point=getRandomInteger(6, 1);
+	System.out.println("Player go forward by "+point+ " point" );
 }
+public static int getRandomInteger(int maximum, int minimum){
+		 return ((int) (Math.random()*(maximum - minimum))) + minimum; 
+	}
 }
