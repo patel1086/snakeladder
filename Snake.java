b32a88c94f9c1a3160bf64718817f72345b9bb16
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-09-24 11:04:14 +0530

    [Jitendra] Added : Check for option

--- Snake.java
+++ Snake.java
@@ -3,7 +3,18 @@ public class Snake{
  	int player=0;
 	System.out.println("Player is at starting point");
 	int point=getRandomInteger(6, 1);
-	System.out.println("Player go forward by "+point+ " point" );
+	//System.out.println("Player go forward by "+point+ " point" );
+	int No_play=1;
+	int ladder=2;
+	int snake=3;
+        int action=getRandomInteger(3,1);
+        if(action==No_play)
+		player+=0;
+        else if(action==ladder)
+                player+=point;
+	else 
+		player-=point;
+                
 }
 public static int getRandomInteger(int maximum, int minimum){
 		 return ((int) (Math.random()*(maximum - minimum))) + minimum; 
