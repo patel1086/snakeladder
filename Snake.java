02b68e1e9e8a137913be9794dd5351ae3c700a08
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-09-24 11:08:54 +0530

    [Jitendra] Added: Player move less than zero then restart

--- Snake.java
+++ Snake.java
@@ -2,18 +2,26 @@ public class Snake{
 	public static void main(String [] args){
  	int player=0;
 	System.out.println("Player is at starting point");
-	int point=getRandomInteger(6, 1);
+	
 	//System.out.println("Player go forward by "+point+ " point" );
 	int No_play=1;
 	int ladder=2;
 	int snake=3;
-        int action=getRandomInteger(3,1);
-        if(action==No_play)
-		player+=0;
-        else if(action==ladder)
-                player+=point;
-	else 
-		player-=point;
+        
+	for(player=0;player<=100;){
+		int point=getRandomInteger(6, 1);
+		int action=getRandomInteger(3,1);
+		
+        	if(action==No_play)
+			player+=0;
+        	else if(action==snake){
+                	player-=point;
+			if(player<0)
+				player=0;
+		}
+		else 
+			player+=point;
+	}
                 
 }
 public static int getRandomInteger(int maximum, int minimum){
